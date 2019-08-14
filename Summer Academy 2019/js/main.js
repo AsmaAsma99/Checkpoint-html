90e330417def367855b0b3f98dd84d79ff21ac4a
AsmaAsma99 <53865933+AsmaAsma99@users.noreply.github.com>
2019-08-14 13:49:11 +0100

    Add files via upload

---
+++ Summer Academy 2019/js/main.js
@@ -0,0 +1,32 @@
+var x=document.getElementById("textarea");
+var y=document.getElementById("size");
+var z=document.getElementById("police");
+
+function bold(){
+    if (x.style.fontWeight!="bold")
+        x.style.fontWeight="bold";
+        else
+            x.style.fontWeight="normal";   
+}
+
+function Italic() {
+    if (x.style.fontStyle!="italic")
+       x.style.fontStyle="italic";
+       else
+           x.style.fontStyle="normal";
+}
+
+function Underlinetext() {
+    if (x.style.textDecoration!="underline")
+       x.style.textDecoration="underline";
+       else
+           x.style.textDecoration="none";
+}
+
+function changeSize() {
+    x.style.fontSize=y.value;
+}
+
+function changePolice() {
+    x.style.fontFamily=z.value;
+}
